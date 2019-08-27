eca2ab08d6f000b641c1d720ab503494e4e5c018
Collin Lee <cstlee@cs.stanford.edu>
2019-08-27 13:36:09 -0700

    Add gen-smux-config

--- scripts/gen-smux-config.sh
+++ scripts/gen-smux-config.sh
@@ -2,8 +2,12 @@
 
 RC_HOSTS_FILE=$1
 
+echo "PANES_PER_WINDOW = $(wc -l < $RC_HOSTS_FILE)"
+echo "LAYOUT = tiled"
+
 while read HOST; do
+	echo "------"
     HOST_PUBLIC_IP=`geni-get manifest | grep $HOST | egrep -o "ipv4=.*" | cut -d'"' -f2`
-    echo "$HOST $HOST_PUBLIC_IP"
+    echo "ssh $HOST_PUBLIC_IP"
 done < $RC_HOSTS_FILE
 
